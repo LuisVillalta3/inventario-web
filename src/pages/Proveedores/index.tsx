c8ac444864e785e311c87f6045cb403e9e1ac441
LuisVillalta3 <luisvillalta3@gmail.com>
2023-06-05 20:16:58 -0600

    Flex columns

--- src/pages/Proveedores/index.tsx
+++ src/pages/Proveedores/index.tsx
@@ -6,16 +6,17 @@ import { GridColDef } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID" },
-  { field: "idProveedor", headerName: "Código" },
-  { field: "nombre", headerName: "Nombre" },
+  { field: "id", headerName: "ID", flex: 1 },
+  { field: "idProveedor", headerName: "Código", flex: 1 },
+  { field: "nombre", headerName: "Nombre", flex: 1 },
   {
     field: "email",
     headerName: "Email",
+    flex: 1,
   },
-  { field: "direccion", headerName: "Dirección" },
-  { field: "telefono", headerName: "Teléfono" },
-  { field: "fax", headerName: "fax" },
+  { field: "direccion", headerName: "Dirección", flex: 1 },
+  { field: "telefono", headerName: "Teléfono", flex: 1 },
+  { field: "fax", headerName: "fax", flex: 1 },
 ];
 
 const Proveedores = () => {
